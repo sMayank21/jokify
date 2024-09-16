c35d43b10bdbbf0605e15aa26298438eab1a319f
Mayank Swaraj <mayankswaraj@Mayanks-MacBook-Air.local>
2024-09-16 20:02:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import axios from "axios";
+import bodyparser from "body-parser";
+
+const PORT = 3000;
+const app = express();
+const API_URL = "https://v2.jokeapi.dev/joke/"
+
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended: true}));
+
+app.get("/", (req,res) => {
+
+    res.render("index.ejs");
+});
+app.post("/",async(req,res)=>{
+    try{
+        const joke_type = req.body.type;
+        const language = req.body.language;
+        const submit = await axios(API_URL+`/${joke_type}`,{
+            params:{
+               lang: language
+            }
+        });
+        let result = submit.data;
+        if(result.type == 'twopart'){
+            res.render("index.ejs",{setup: result.setup , delivery: result.delivery});
+        } else{
+            res.render("index.ejs",{joke:result.joke});
+        }
+    } catch(error){
+        res.send(error.message);
+    }
+});
+
+app.listen(PORT,()=>{
+    console.log(`listing on port: ${PORT}`);
+});
